bc74e4fc665b76fc49df1c4b036bef93e5546ab1
Bento <david.bento-pereira@lilo.org>
2024-07-01 11:55:09 +0200

    WIP

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,13 +1,42 @@
-export default function Home({wines}) {
+import { useState, useEffect } from 'react'
+
+// export default function Home({wines}) {
+export default function Home() {
+  const [wines, setWines] = useState([])
+
+  useEffect(() => {
+    fetch('http://localhost:3000/wines')
+      .then((res) => res.json())
+      .then((data) => {
+        setWines(data);
+      })
+  }, [])
+
+  function filterWines(value) {
+    const minPrice = value
+
+    fetch(`http://localhost:3000/wines?min_price=${minPrice}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setWines(data);
+      })
+  }
+
   return (
     <div>
-      <h1 className="mb-8">List of Wines</h1>
+      <h1 className="mb-8">Wines</h1>
+
+      <div className="text-gray-700">
+        <input type="number" id="minPrice" name="minPrice" placeholder="Min Price" className="mr-2" onChange={(e) => {filterWines(e.target.value);}} />
+        <input type="number" id="maxPrice" name="maxPrice" placeholder="Max Price"/>
+      </div>
+
       <hr></hr>
       <ul>
         {wines.map((wine) => (
           <li key={wine.id} className="mb-3">
             <h2>{wine.name}</h2>
-            <p>Price: ${wine.price}</p>
+            <p>Price: {wine.price}€</p>
             <p>Average Review: {wine.average_review}</p>
           </li>
         ))}
@@ -26,6 +55,6 @@ export async function getServerSideProps() {
   const wines = await res.json();
 
   return {
-    props: { wines }, // Les props seront passées au composant de la page
+    props: { wines }
   };
 }
